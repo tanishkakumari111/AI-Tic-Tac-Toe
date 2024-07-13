19b5a5775928a31b8a2ba95b750790af35549f1c
tanishkakumari111 <168574561+tanishkakumari111@users.noreply.github.com>
2024-07-13 11:12:00 +0530

    checkForDraw() added
    >>checks for all the draw condition
    >>Also: displaying "Player Won" and "Game over" messages onto the frame 1 over the Title

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ root.title("Tic Tac Toe")
 
 frame1 = Frame(root)
 frame1.pack()
-titleLabel = Label(frame1, text="Tic Tac Toe", font=("Arial", 20), bg="yellow")
-titleLabel.pack(side=TOP)
+titleLabel = Label(frame1, text="Tic Tac Toe", font=("Arial", 30), bg="lightpink")
+titleLabel.grid(row=0, column=0)
 
 frame2 = Frame(root)
 frame2.pack()
@@ -41,6 +41,12 @@ def checkForWin(player):
         return True
     return False
 
+def checkForDraw():
+    for i in board.keys():
+        if board[i] == " ":
+            return False
+    return True
+
 def play(event):
     global turn
     button = event.widget
@@ -57,60 +63,69 @@ def play(event):
             button["text"]="X"
             board[clicked] = turn
             if checkForWin(turn): 
-                WinningLabel = Label(frame2, text=f"{turn} wins the game", bg="orange", font=("Arial", 20))
-                WinningLabel.grid(row=3, column=0, columnspan=3)
+                WinningLabel = Label(frame1, text=f"{turn} wins the game", bg="coral", font=("Arial", 30))
+                WinningLabel.grid(row=0, column=0, columnspan=3)
             turn = "O"
+
         else:
             button["text"]="O"
             board[clicked] = turn
             if checkForWin(turn): 
-                WinningLabel = Label(frame2, text=f"{turn} wins the game", bg="orange", font=("Arial", 20))
-                WinningLabel.grid(row=3, column=0, columnspan=3)
+                WinningLabel = Label(frame1, text=f"{turn} wins the game", bg="orange", font=("Arial", 30))
+                WinningLabel.grid(row=0, column=0, columnspan=3)
             turn = "X"
+
+        if checkForDraw():
+            DrawLabel = Label(frame2, text="Game draw", bg="orange", font=("Arial", 20))
+            DrawLabel.grid(row=3, column=0, columnspan=3)
 
 #tic tac toe board
 
 #first row
 
-button1 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button1 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button1.grid(row=0, column=0)
 button1.bind("<Button-1>", play)
 
-button2 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button2 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button2.grid(row=0, column=1)
 button2.bind("<Button-1>", play)
 
-button3 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button3 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button3.grid(row=0, column=2)
 button3.bind("<Button-1>", play)
 
 #second row
 
-button4 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button4 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button4.grid(row=1, column=0)
 button4.bind("<Button-1>", play)
 
-button5 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button5 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button5.grid(row=1, column=1)
 button5.bind("<Button-1>", play)
 
-button6 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button6 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button6.grid(row=1, column=2)
 button6.bind("<Button-1>", play)
 
 #third row
 
-button7 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button7 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button7.grid(row=2, column=0)
 button7.bind("<Button-1>", play)
 
-button8 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button8 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button8.grid(row=2, column=1)
 button8.bind("<Button-1>", play)
 
-button9 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=3)
+button9 = Button(frame2, text=" ", width=4, height=2, font=("Arial", 30), bg="lightblue", relief=RAISED, borderwidth=5)
 button9.grid(row=2, column=2)
 button9.bind("<Button-1>", play)
+
+
+restartButton = Button(frame2, text="Restart Game", width=12, height=1, font=("Arial", 20), bg="lightgreen", relief=RAISED, border=5)
+restartButton.grid(row=4, column=0, columnspan=3)
 
 
 root.mainloop()
